64aad64c33ed9c372e947276d034952b3abd5678
Kang-Se <tpwns3382@likelion.org>
2020-08-04 10:00:31 +0900

    08.04

--- virtual/crawl2.py
+++ virtual/crawl2.py
@@ -15,7 +15,7 @@ for movie in movie_section:
     a_tag = movie.select_one('dl > dt > a')
     movie_title = (str(a_tag).split('>')[1]).split('<')[0]
     movie_code = (a_tag['href'].split('?')[1]).split('=')[1]
-    movie_dict["code"] = str(movie_code)
-    movie_dict["title"] = str(movie_title)
+    movie_dict["code"] = movie_code
+    movie_dict["title"] = movie_title
     movie_data.append(movie_dict)
 print(movie_data)
